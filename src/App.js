573211dfbbc07dcc06d1b294aa628f3329567427
jselga <jordi.selga@iescarelesvallbona.cat>
2024-11-06 12:11:49 +0100

    anatomia de components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+const Li = ({children})=>{
+  // console.log(props);
+  
+  return(
+    <li className='clase-li'>{children}</li>
+  )
+}
+const App = () => {
+  const valor ='triste'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className='clase-css'>
+      <Li estado="feliz">valor de li</Li>
+    </ul>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,35 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-const Li = ({ children, estado,casa,edad }) => {
-  console.log(casa,edad);
-  
-  return (
-    <li>{children} estoy {estado}</li>
-  )
-}
-
-const X = () =>
-  <ul>
-    <Li
-      estado={'feliz'}
-      casa={false}
-      edad={24}
-    >
-      Chanchito
-    </Li>
-    
-    <Li estado={'triste'}>Chanchito</Li>
-    <Li estado={'triste'}>Felipe</Li>
-  </ul>
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<X />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+// const Li = ({ children, estado,casa,edad }) => {
+//   console.log(casa,edad);
+  
+//   return (
+//     <li>{children} estoy {estado}</li>
+//   )
+// }
+
+// const X = () =>
+//   <ul>
+//     <Li
+//       estado={'feliz'}
+//       casa={false}
+//       edad={24}
+//     >
+//       Chanchito
+//     </Li>
+    
+//     <Li estado={'triste'}>Chanchito</Li>
+//     <Li estado={'triste'}>Felipe</Li>
+//   </ul>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<X />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
