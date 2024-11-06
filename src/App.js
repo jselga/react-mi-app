c50c9f5344773cdad7f9a3a3ef16b79a85df54b1
jselga <jordi.selga@iescarelesvallbona.cat>
2024-11-06 13:37:17 +0100

    Reutilitzant components amb estils

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import logo from './logo.svg';
-// import './App.css';
-import './main.css';
-
-const estilo2 ={
-  boxShadow:'0 5px 3px rgba(0,0,0,0.5)'
-}
-const estilo = ({bg = '#222'}) => ({
-  backgroundColor: bg,
-  color: '#fff',
-  padding: '10px 15px',
-  margin: '10px 15px',
-
-})
-const Li = ({ children }) => {
-  // console.log(props);
-
-  return (
-    <li className='clase-li'>{children}</li>
-  )
-}
+import React from 'react'
+import Button from './Button'
 const App = () => {
-  const valor = 'triste'
-  return (
-    <ul className='clase-css'>
-      <Li estado="feliz">valor de li</Li>
-    </ul>
-  );
+    return (
+        <div>
+            <h1>
+                Hola Mundo
+            </h1>
+            <Button onClick={() => console.log('clickeado')}>
+                Enviar
+            </Button>
+
+
+
+
+        </div>
+    )
 }
 
-export default App;
+export default App
