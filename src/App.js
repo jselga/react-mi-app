559c13eea6afb6a7fb62cce26cc3e321bf2ce462
jselga <jordi.selga@iescarelesvallbona.cat>
2024-11-06 12:24:25 +0100

    inline styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-const Li = ({children})=>{
+const estilo2 ={
+  boxShadow:'0 5px 3px rgba(0,0,0,0.5)'
+}
+const estilo = ({bg = '#222'}) => ({
+  backgroundColor: bg,
+  color: '#fff',
+  padding: '10px 15px',
+  margin: '10px 15px',
+
+})
+const Li = ({ children }) => {
   // console.log(props);
-  
-  return(
-    <li className='clase-li'>{children}</li>
+
+  return (
+    <li style={{...estilo2,...estilo({bg:'#333'})}} className='clase-li'>{children}</li>
   )
 }
 const App = () => {
-  const valor ='triste'
+  const valor = 'triste'
   return (
-    <ul className='clase-css'>
+    <ul style={estilo({bg:'#750'})} className='clase-css'>
       <Li estado="feliz">valor de li</Li>
     </ul>
   );
